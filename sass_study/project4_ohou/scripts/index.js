e879f41abc4937e002a4b64b1270736664247776
no-okane <dnjsgml3304.15@gmail.com>
2025-09-19 11:19:28 +0900

    오늘의 집 쇼핑 상품 완성

--- sass_study/project4_ohou/scripts/index.js
+++ sass_study/project4_ohou/scripts/index.js
@@ -229,3 +229,64 @@ closeBtn.addEventListener('click',function(){
     colorSelect.selectedIndex = colorSelect.options[0]
     sizeSelect.selectedIndex = sizeSelect.options[0]
 }) /* 에러난 거 확인하기 */
+
+// 9. 주문목록 + 클릭 시 재고수량까지 주문수량 + 주문금액 표시
+// 필요목록 : +버튼, 재고수량(productOptDB[0].stock), 주문수량, 주문금액(orderPrice), 증가 숫자 데이터
+const plusBtn = document.querySelector('#plus_btn')
+const minusBtn = document.querySelector('#minus_btn')
+const orderNum = document.querySelector('#order_num')
+const orderListPrice = document.querySelector('.order_list .price')
+let num = 1; // 초기주문수량
+
+// 초기값 : 주문 수량칸에 값 1 적용하기
+orderNum.value = num; // 수량칸에 직접 입력이 불가하면 textContent
+
+// +버튼 클릭 시 주문수량이 1씩 증가하고 주문수량에 따라 가격(productOptDB[0].price) 증가하기
+plusBtn.addEventListener('click',()=>{
+    if(num < productOptDB[0].stock){
+    num++;
+    minusPlusfunc()
+    }else {
+        alert('최대 구매 수량입니다.')
+    }
+})
+// 10. 주문목록 - 클릭 시 주문수량 + 주문금액 감소(수량이 1이라면 경고창(팝업) 출력)
+minusBtn.addEventListener('click',()=>{
+    if(num > 0){
+    num--;
+    minusPlusfunc() // 함수호출
+    }else {
+        alert('최소 구매 수량입니다.')
+    }
+})
+function minusPlusfunc (){ // 함수생성
+    let total = num * productOptDB[0].price
+    orderListPrice.textContent = total.toLocaleString('ko-kr') // 3자리 수마다 콤마
+    totalPrice.textContent = total.toLocaleString('ko-kr') // html 선택자명 확인할 것
+    return;
+}
+
+// 11. (상품 미선택 시) 장바구니, 바로구매 클릭 시 '상품을 선택하세요' 경고창(팝업) 출력
+// 12. 😄 (상품 선택 시) 장바구니, 바로구매 클릭 시 로그인 유무에 따라 다른 페이지로 이동
+const cartBtn = document.querySelector('#cart_btn')
+const buyBtn = document.querySelector('#buy_btn')
+console.log(carBtn, buyBtn)
+
+cartBtn.addEventListener('click',()=>{
+    cartBuyFunc('./cart.html')
+})
+buyBtn.addEventListener('click',()=>{
+    cartBuyFunc('./buy.html')
+})
+
+function cartBuyFunc(url){
+    if(colorSelect.selectedIndex == 0 || sizeSelect.selectedIndex == 0){ // 상품 선택 안 한 걸 확인하는 조건문
+        alert('옵션 선택 후에 버튼을 클릭해주세요.')
+    }else {
+        // 장바구니 페이지 이동(로그인 유(장바구니) 무(로그인)에 따라)
+        loginStatus = localStorage.getItem('isLogin')
+        if(loginStatus == 'true'){
+            location.href = url
+        }else {location.href = './login.html'}
+    }
+}
