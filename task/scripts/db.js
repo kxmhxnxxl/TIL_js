14b221f8d02936ca55b37729345120e47efe6a45
no-okane <dnjsgml3304.15@gmail.com>
2025-09-25 12:49:45 +0900

    탄생화 찾기 js+jq

---
+++ task/scripts/db.js
@@ -0,0 +1,63 @@
+const birthday_flower = [
+    {
+        month:1,
+        flower:'스노우드롭',
+        content:'깊은 애정',
+        src:'https://i.pinimg.com/736x/a7/1e/64/a71e64fc794e81d003f76c5df3c94da5.jpg'
+    },{
+        month:2,
+        flower:'제비꽃',
+        content:'겸손, 양보',
+        src:'https://i.pinimg.com/736x/d0/dc/ad/d0dcaddf9eee98827e0b64879467e9de.jpg'
+    },{
+        month:3,
+        flower:'수선화',
+        content:'자기애, 자존심, 외로움',
+        src:'https://i.pinimg.com/736x/0e/16/bc/0e16bc0eb865e2579a5e341fed26982f.jpg'
+    },{
+        month:4,
+        flower:'스위트피',
+        content:'추억, 즐거움',
+        src:'https://i.pinimg.com/736x/1a/f0/1d/1af01d04e1f051d8a1e113b65ee88d04.jpg'
+    },{
+        month:5,
+        flower:'은방울꽃',
+        content:'희망, 섬세함',
+        src:'https://i.pinimg.com/736x/77/f4/35/77f4358eb4c867354c38e4173fdca2db.jpg'
+    },{
+        month:6,
+        flower:'백합',
+        content:'순결',
+        src:'https://i.pinimg.com/736x/c3/6b/f2/c36bf2d3bb6aea818f517d5a085618aa.jpg'
+    },{
+        month:7,
+        flower:'미나리아재비',
+        content:'아름다움, 인격',
+        src:'https://i.pinimg.com/736x/72/a8/bb/72a8bb38ff52d986098263eed3cbd48d.jpg'
+    },{
+        month:8,
+        flower:'글라디올러스',
+        content:'비밀, 상상, 견고함',
+        src:'https://i.pinimg.com/1200x/d2/c3/3e/d2c33eeaba75b2a6edc56415a46e27dc.jpg'
+    },{
+        month:9,
+        flower:'물망초',
+        content:'진실한 사랑',
+        src:'https://i.pinimg.com/736x/af/94/55/af9455a2d6932dbaf19ae47594cacf94.jpg'
+    },{
+        month:10,
+        flower:'금잔화',
+        content:'실망, 비애',
+        src:'https://i.pinimg.com/1200x/45/c4/ea/45c4ea75853d7671dd60a5344177a558.jpg'
+    },{
+        month:11,
+        flower:'국화',
+        content:'성실, 진실',
+        src:'https://i.pinimg.com/736x/75/bc/e0/75bce034063afc43c98023dba7eb457f.jpg'
+    },{
+        month:12,
+        flower:'포인세티아',
+        content:'축하, 감사',
+        src:'https://i.pinimg.com/736x/f6/3e/f5/f63ef5a5690eda5bb50f2ec05689cbdb.jpg'
+    }
+]
